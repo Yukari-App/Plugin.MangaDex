7a492944a66fa03af91d97a1dc66e6f5953ae5f6
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-17 16:26:31 -0300

    feat: add initial project files

---
+++ Yukari.Plugin.MangaDex/MangaDexSource.cs
@@ -0,0 +1,16 @@
+﻿using Yukari.Core.Models;
+using Yukari.Core.Sources;
+
+namespace Yukari.Plugin.MangaDex
+{
+    public class MangaDexSource : IMangaSource
+    {
+        public string Name => "MangaDex";
+
+        public Task<List<Comic>> SearchAsync(string query) => throw new NotImplementedException();
+        public Task<List<Comic>> GetTrendingAsync() => throw new NotImplementedException();
+        public Task<Comic?> GetDetailsAsync(string mangaId) => throw new NotImplementedException();
+        public Task<List<ChapterPage>> GetChapterPagesAsync(string chapterId) => throw new NotImplementedException();
+        public Task<List<Chapter>> GetAllChaptersAsync(string mangaId, string language) => throw new NotImplementedException();
+    }
+}
