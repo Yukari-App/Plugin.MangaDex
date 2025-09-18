b359f6282b50516e977df595b8c25e162bdf840d
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-18 18:21:34 -0300

    feat: add Cover data models for API responses

---
+++ Yukari.Plugin.MangaDex/Data/Cover.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Yukari.Plugin.MangaDex.Data
+{
+    internal record Cover(
+            [property: JsonPropertyName("attributes")] CoverAttributes Attributes
+        );
+}
---
+++ Yukari.Plugin.MangaDex/Data/CoverResponse.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Yukari.Plugin.MangaDex.Data
+{
+    internal record CoverResponse(
+            [property: JsonPropertyName("data")] Cover Data
+        );
+}
