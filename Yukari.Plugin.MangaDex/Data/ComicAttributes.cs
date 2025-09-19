896d3f1199bc0c78c98a7b7f5a352798be2eff10
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-19 00:24:21 -0300

    feat: allow Year to be nullable in ComicAttributes

--- Yukari.Plugin.MangaDex/Data/ComicAttributes.cs
+++ Yukari.Plugin.MangaDex/Data/ComicAttributes.cs
@@ -7,7 +7,7 @@ namespace Yukari.Plugin.MangaDex.Data
             [property: JsonPropertyName("title")] Dictionary<string, string> Title,
             [property: JsonPropertyName("altTitles")] List<Dictionary<string, string>> AltTitles,
             [property: JsonPropertyName("description")] Dictionary<string, string> Description,
-            [property: JsonPropertyName("year")] int Year,
+            [property: JsonPropertyName("year")] int? Year,
             [property: JsonPropertyName("tags")] Tag[] Tags,
             [property: JsonPropertyName("availableTranslatedLanguages")] string[] Languages
         );
