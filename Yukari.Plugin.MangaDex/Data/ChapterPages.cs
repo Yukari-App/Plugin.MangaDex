a40b747066b66a590e62ba99eec3f01d63586f75
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-30 03:20:52 -0300

    feat: add Hash property

--- Yukari.Plugin.MangaDex/Data/ChapterPages.cs
+++ Yukari.Plugin.MangaDex/Data/ChapterPages.cs
@@ -3,6 +3,7 @@ using System.Text.Json.Serialization;
 namespace Yukari.Plugin.MangaDex.Data
 {
     internal record ChapterPages(
+            [property: JsonPropertyName("hash")] string Hash,
             [property: JsonPropertyName("data")] string[] Data,
             [property: JsonPropertyName("dataSaver")] string[] DataSaver
         );
