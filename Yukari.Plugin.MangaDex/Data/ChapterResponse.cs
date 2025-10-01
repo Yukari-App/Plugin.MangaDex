23464cbab0b137d2e609c685967f7514423fbca0
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-30 21:02:36 -0300

    feat: add Total and Offset properties to ChapterResponse

--- Yukari.Plugin.MangaDex/Data/ChapterResponse.cs
+++ Yukari.Plugin.MangaDex/Data/ChapterResponse.cs
@@ -3,6 +3,8 @@ using System.Text.Json.Serialization;
 namespace Yukari.Plugin.MangaDex.Data
 {
     internal record ChapterResponse(
-            [property: JsonPropertyName("data")] MangaDexChapter[] Data
+            [property: JsonPropertyName("data")] MangaDexChapter[] Data,
+            [property: JsonPropertyName("offset")] int Offset,
+            [property: JsonPropertyName("total")] int Total
         );
 }
