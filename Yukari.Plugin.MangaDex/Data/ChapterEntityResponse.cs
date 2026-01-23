e91f49e590a43c4dae13a91de1dd41ff46977f87
TXG0Fk3 <leovertonx12@outlook.com>
2026-01-23 16:45:52 -0300

    feat(data): add ChapterEntityResponse DTO

---
+++ Yukari.Plugin.MangaDex/Data/ChapterEntityResponse.cs
@@ -0,0 +1,8 @@
+using System.Text.Json.Serialization;
+
+namespace Yukari.Plugin.MangaDex.Data
+{
+    internal record ChapterEntityResponse(
+            [property: JsonPropertyName("data")] MangaDexChapter Data
+        );
+}
